322af5c115699ecf900145615edf3767d95dce50
admin <admin@admin.com>
2017-11-05 23:08:38 +0100

    Unitix
    Unitix source code

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define unitix-qt message queue name
+#define BITCOINURI_QUEUE_NAME "unitixURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
